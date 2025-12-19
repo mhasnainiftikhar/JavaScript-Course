3b184dd32341301d7dfd73539e2bbc137a5bf560
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-19 15:22:52 +0500

    Values and Variables

---
+++ Fundamentals01/ValuesAndVariables.js
@@ -0,0 +1,28 @@
+//-------------Values-------------//
+//"Hasnain" is a Value
+//23 is Value
+
+console.log("Hasnain")
+console.log(23)
+
+//---------------Variables----------------
+//variables are use to store the values and later we use this in different places
+
+let firstName='Hasnain';
+let age=24;
+let email='hasnain@gmail.com';
+
+console.log(firstName);
+console.log(age);
+console.log(email);
+
+let country = 'Pakistan';
+let continent = 'Aasia';
+let population = 28;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//In JS we use Camel convention to written the name of variables
+//Also some words like new,function,name are resevred in the JS we not use them as a variable name
