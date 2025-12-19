45735e0e2aa6bddb0fb7a97b6bac066e59eb76f8
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-19 15:54:22 +0500

    Primitive Datatypes in JavaScript

---
+++ Fundamentals01/DataTypes.js
@@ -0,0 +1,28 @@
+/*Datatypes are the way to define type of values that we store in variables.
+There are two types of Datatypes->Object(Non Primitive) & Prirmitive.
+Primitive datatypes are 7
+1.String
+2.Number
+3.Boolean
+4.Undefine
+5.Null
+6.Symbol
+7.Bigint
+*/
+
+let country="Pakistan";  //String
+let age=23;   //Number
+let isVerified=true; //Boolean
+let religion;  //undefined
+let user=null; //null
+
+
+let isIsland = false;
+let language;
+
+//Typeof is use to find the type of data
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof language);
