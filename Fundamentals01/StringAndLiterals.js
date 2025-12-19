27e949eef68c5b0c7d11cd62f3baed1269a1f5f6
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-19 17:44:39 +0500

    Strings and Template literals

---
+++ Fundamentals01/StringAndLiterals.js
@@ -0,0 +1,20 @@
+//The concatenation of string
+let firstName='Hasnain'
+let age=23;
+let job="Engineer"
+const bio='I am '+ firstName +', a '+age+' year old'+job
+console.log(bio)
+
+
+//Through the Help of Literals
+let intro='Ali Raza';
+let experience=5;
+let designation='Senior Full Stack developer'
+let from='Lahore'
+
+let profile = `My name is ${intro}.
+I am a ${designation}.
+I have ${experience} years of experience.
+I am from ${from}.`;
+
+console.log(profile);
