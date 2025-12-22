c611c2e2e23a5f9ea90fa46bce879c074e54e74e
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-22 10:23:23 +0500

    Use of Equal Operators

---
+++ Fundamentals01/EqualOperator.js
@@ -0,0 +1,16 @@
+/* 
+ (==) Compares only values
+ (===) Compares value AND data type
+*/
+
+// (==)
+5 == "5"   // true
+// (===)
+5 == "5"   // false
+
+console.log(10 == "10");    // true
+console.log(10 === "10");   // false
+
+console.log(true == 1);     // true
+console.log(true === 1);    // false
+
