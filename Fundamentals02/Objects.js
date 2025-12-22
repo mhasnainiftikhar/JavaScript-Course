e0cf1eaf04fda1aaae6bd6d0df80a95aa261d92e
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-22 17:52:36 +0500

    Objects in JS

---
+++ Fundamentals02/Objects.js
@@ -0,0 +1,9 @@
+//A variable that store many variables on the Key value pairs
+const bio={
+    firstName:'Hasnain',
+    age:24,
+    email:'hasnainiftikhar930@gmail.com',
+    address:"Lahore"
+}
+
+console.log(bio)
