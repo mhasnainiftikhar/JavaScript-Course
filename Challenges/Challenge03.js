22ae48d689747a3213f69bfb9c99feed3e8d77cc
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-22 12:48:07 +0500

    With Bonus Data

--- Challenges/Challenge03.js
+++ Challenges/Challenge03.js
@@ -14,3 +14,45 @@ if (dolphinsAvg > koalasAvg) {
 } else {
   console.log("It's a draw ..");
 }
+
+// Bonus 1
+let dolphinsAvgBonus1 = calcAverage(97, 112, 101);
+let koalasAvgBonus1 = calcAverage(109, 95, 123);
+
+console.log("Dolphins Average:", dolphinsAvgBonus1);
+console.log("Koalas Average:", koalasAvgBonus1);
+
+if (dolphinsAvgBonus1 > koalasAvgBonus1 && dolphinsAvgBonus1 >= 100) {
+  console.log("Dolphins win the trophy ");
+} else if (koalasAvgBonus1 > dolphinsAvgBonus1 && koalasAvgBonus1 >= 100) {
+  console.log("Koalas win the trophy ");
+} else {
+  console.log("No team wins the trophy ");
+}
+
+// Bonus 2
+let dolphinsAvgBonus2 = calcAverage(97, 112, 101);
+let koalasAvgBonus2 = calcAverage(109, 95, 106);
+
+console.log("Dolphins Average:", dolphinsAvgBonus2);
+console.log("Koalas Average:", koalasAvgBonus2);
+
+if (
+  dolphinsAvgBonus2 > koalasAvgBonus2 &&
+  dolphinsAvgBonus2 >= 100
+) {
+  console.log("Dolphins win the trophy ");
+} else if (
+  koalasAvgBonus2 > dolphinsAvgBonus2 &&
+  koalasAvgBonus2 >= 100
+) {
+  console.log("Koalas win the trophy ");
+} else if (
+  dolphinsAvgBonus2 === koalasAvgBonus2 &&
+  dolphinsAvgBonus2 >= 110 &&
+  koalasAvgBonus2 >= 100
+) {
+  console.log("It's a draw ");
+} else {
+  console.log("No team wins the trophy ");
+}
