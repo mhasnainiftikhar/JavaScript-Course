deb6993736e42845a1e7fca7f4a3a44acc8e2f97
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-22 13:00:40 +0500

    Use of Switch Statement in JS

---
+++ Fundamentals01/SwitchStatement.js
@@ -0,0 +1,19 @@
+//A switch statement is used to check one value against many possible cases
+let day = 4;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  default:
+    console.log("Invalid day");
+}
