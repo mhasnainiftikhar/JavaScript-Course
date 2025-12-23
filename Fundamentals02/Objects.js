e549fe52c2dc0a2411cbc7935fddccce7443f4f4
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-23 11:12:43 +0500

    Methods of Object in JS

--- Fundamentals02/Objects.js
+++ Fundamentals02/Objects.js
@@ -1,20 +1,37 @@
-//A variable that store many variables on the Key value pairs
-const bio={
-    firstName:'Hasnain',
-    age:24,
-    email:'hasnainiftikhar930@gmail.com',
-    address:"Lahore",
-    "home city": "Pattoki"
+// Object -> A variable that stores many values/Variables in key-value pairs
+const bio = {
+  firstName: 'Hasnain',
+  age: 24,
+  email: 'hasnainiftikhar930@gmail.com',
+  address: 'Lahore',
+  "home city": "Pattoki",
 
-}
-console.log(bio)
+  getInfo: function () {
+    return `My name is ${this.firstName}. I am ${this.age} years old and I live in ${this.address}.`;
+  },
 
-//--------------------dot notation and bracket notation in JavaScript objects-----------
+  updateAge: function (newAge) {
+    this.age = newAge;
+  }
+};
 
-// Dot -> Used when you know the property name.
-console.log(bio.firstName); 
+console.log(bio);
+
+// Dot notation(Used when you know the property name.)
+console.log(bio.firstName);
 console.log(bio.age);
 
-// Bracket -> Used when the property name is in a variable or has spaces/special characters.
+// Bracket notation(Used when the property name is in a variable or has spaces/special characters.)
+console.log(bio["home city"]);
 
-console.log(bio['home city'])
+// Using object methods
+console.log(bio.getInfo());
+
+// Updating value using 'this'
+bio.updateAge(25);
+console.log("Updated Age:", bio.age);
+
+// Built-in object methods
+console.log(Object.keys(bio));
+console.log(Object.values(bio));
+console.log(Object.entries(bio));
