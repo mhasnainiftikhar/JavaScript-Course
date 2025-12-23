5ee1ffe4fe8bf56534ff0b6c8fa9eaa49eeb241c
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-23 12:01:52 +0500

    While loop in JS

--- Fundamentals02/Loops.js
+++ Fundamentals02/Loops.js
@@ -25,3 +25,12 @@ for (let i = 0; i < names.length; i++) {
 for (let i=names.length-1; i>=0; i--){
     console.log(names[i]);
 }
+
+
+//While loop
+let count = 1;
+
+while (count <= 15) {
+  console.log(`While loop ${count}`);
+  count++;
+}
