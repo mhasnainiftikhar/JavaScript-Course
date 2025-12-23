ee7cfb6730daa7ec3ad29cad4ea6c0cf1557f26b
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2025-12-23 10:39:32 +0500

    Dot notation and bracket notation in JavaScript objects

--- Fundamentals02/Objects.js
+++ Fundamentals02/Objects.js
@@ -3,7 +3,18 @@ const bio={
     firstName:'Hasnain',
     age:24,
     email:'hasnainiftikhar930@gmail.com',
-    address:"Lahore"
-}
+    address:"Lahore",
+    "home city": "Pattoki"
 
+}
 console.log(bio)
+
+//--------------------dot notation and bracket notation in JavaScript objects-----------
+
+// Dot -> Used when you know the property name.
+console.log(bio.firstName); 
+console.log(bio.age);
+
+// Bracket -> Used when the property name is in a variable or has spaces/special characters.
+
+console.log(bio['home city'])
